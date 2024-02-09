3c601fd4ab4074cbec07e638fcb0e434f31cafe2
Hernan Tierno Fernandez <hernantiefer@campus.monlau.com>
2024-02-09 19:19:18 +0100

    Primer video acabado

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,13 +3,37 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout player1Layout, player2Layout;
+    private ImageView image1, image2, image3, image4, image5, image6, image7, image8, image9;
+    private TextView player1TV, player2TV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        player1Layout = findViewById(R.id.player1Layout);
+        player2Layout = findViewById(R.id.player2Layout);
+
+        image1 = findViewById(R.id.image1);
+        image2 = findViewById(R.id.image2);
+        image3 = findViewById(R.id.image3);
+        image4 = findViewById(R.id.image4);
+        image5 = findViewById(R.id.image5);
+        image6 = findViewById(R.id.image6);
+        image7 = findViewById(R.id.image7);
+        image8 = findViewById(R.id.image8);
+        image9 = findViewById(R.id.image9);
+
+        player1TV = findViewById(R.id.player1TV);
+        player2TV = findViewById(R.id.player2TV);
+
 
         //obteniendo el PlayerName del archivo PlayerName.class
         final String getPlayerName = getIntent().getStringExtra("playerName");
